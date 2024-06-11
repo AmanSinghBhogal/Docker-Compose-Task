6705d772e4064f23fc91451b47321c47ccf7500e
Aman Singh Bhogal <bhogalamansingh22@gmail.com>
2024-06-11 15:14:52 +0530

    Updated #4

--- api/app.js
+++ api/app.js
@@ -36,6 +36,6 @@ app.use((err, req, res, next) => {
 });
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server Running on port ${process.env.PORT}`)
+app.listen(5000, () => {
+    console.log(`Server Running on port 5000`)
 });
